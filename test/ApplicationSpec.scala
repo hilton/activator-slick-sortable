702347512423cb085439259700b777fa0586cabc
Peter Hilton <hilton@users.noreply.github.com>
2014-07-25 09:48:30 +0100

    Add model tests

---
+++ test/ApplicationSpec.scala
@@ -0,0 +1,43 @@
+import models.Animal
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+ * Test repositioning operations.
+ */
+class ApplicationSpec extends Specification {
+
+  "The model" should {
+    "initially contain sorted names" in
+      new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase("test"))) {
+
+        Animal.list.take(3).map(_.name) must beEqualTo(List("Alligator", "Bear", "Cat"))
+    }
+
+    "allow repositioning" in
+      new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase("test"))) {
+
+        Animal.reposition(1, 2)
+        Animal.list.take(3).map(_.name) must beEqualTo(List("Bear", "Alligator", "Cat"))
+    }
+
+    "allow repositioning backwards" in
+      new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase("test"))) {
+
+        Animal.reposition(3, 1)
+        Animal.list.take(3).map(_.name) must beEqualTo(List("Cat", "Alligator", "Bear"))
+    }
+
+    "retain consecutive positions after repositioning" in
+      new WithApplication(FakeApplication(additionalConfiguration = inMemoryDatabase("test"))) {
+
+        Animal.reposition(1, 2)
+        Animal.reposition(10, 5)
+        Animal.reposition(7, 15)
+        val animals = Animal.list
+        animals.map(_.position) must beEqualTo((1 to animals.size).toList)
+    }
+  }
+}
